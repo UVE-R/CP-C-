1283202a429922517e52286c5ff8aaf993a35f93
UVER <73294472+UVE-R@users.noreply.github.com>
2021-01-07 22:25:19 +0000

    Update UVA10242A- Fourth Point !!.cpp

--- A Problems/UVA10242A- Fourth Point !!.cpp
+++ A Problems/UVA10242A- Fourth Point !!.cpp
@@ -89,20 +89,13 @@ int main()
 
 /*
 while(cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4){
-        if(x1==x3 && y1==y3){
-            
-            cout<<setprecision(3)<<fixed<<x2+x4-x1<<" "<<y2+y4-y1<<endl;
-            
-        }else if(x1==x4 && y1==y4){
-            
-            cout<<setprecision(3)<<fixed<<x2+x3-x1<<" "<<y2+y3-y1<<endl;
-            
+        if(x1==x3 && y1==y3){            
+            cout<<setprecision(3)<<fixed<<x2+x4-x1<<" "<<y2+y4-y1<<endl;            
+        }else if(x1==x4 && y1==y4){            
+            cout<<setprecision(3)<<fixed<<x2+x3-x1<<" "<<y2+y3-y1<<endl;            
         }else if(x2==x3 && y2==y3){
-
-            cout<<setprecision(3)<<fixed<<x1+x4-x2<<" "<<y1+y4-y2<<endl;
-            
-        }else{
-            
+            cout<<setprecision(3)<<fixed<<x1+x4-x2<<" "<<y1+y4-y2<<endl;            
+        }else{            
             cout<<setprecision(3)<<fixed<<x1+x3-x2<<" "<<y1+y3-y2<<endl;
         }
     }
