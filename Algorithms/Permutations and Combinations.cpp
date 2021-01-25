63df005ce38a066f18b213c6f3f220664855a4aa
UVER <73294472+UVE-R@users.noreply.github.com>
2021-01-25 11:12:08 +0000

    Update Permutations and Combinations.cpp

--- Algorithms/Permutations and Combinations.cpp
+++ Algorithms/Permutations and Combinations.cpp
@@ -54,3 +54,61 @@ int main()
     
     return 0;
 }
+
+/*
+void permute(string str, string out)
+{
+    // When size of str becomes 0, out has a
+    // permutation (length of out is n)
+    if (str.size() == 0)
+    {
+        cout << out << endl;
+        return;
+    }
+ 
+    // One be one move all characters at
+    // the beginning of out (or result)
+    for (int i = 0; i < str.size(); i++)
+    {
+        // Remove first character from str and
+        // add it to out
+        permute(str.substr(1), out + str[0]);
+ 
+        // Rotate string in a way second character
+        // moves to the beginning.
+        rotate(str.begin(), str.begin() + 1, str.end());
+    }
+}
+ 
+// Driver code
+int main()
+{
+    string str = "ABC";
+    permute(str, "");
+    return 0;
+}
+*/
+
+/*
+void permute(string str)
+{
+    // Sort the string in lexicographically
+    // ascennding order
+    sort(str.begin(), str.end());
+ 
+    // Keep printing next permutation while there
+    // is next permutation
+    do {
+       cout << str << endl;
+    } while (next_permutation(str.begin(), str.end()));
+}
+ 
+// Driver code
+int main()
+{
+    string str = "CBA";
+    permute(str);
+    return 0;
+}
+*/
+
