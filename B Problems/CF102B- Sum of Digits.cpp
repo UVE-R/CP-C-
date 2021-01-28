869b64213dac6fc4072ccaa67d7a2ecc213adb4b
UVER <73294472+UVE-R@users.noreply.github.com>
2021-01-28 21:42:19 +0000

    Create CF102B- Sum of Digits.cpp

---
+++ B Problems/CF102B- Sum of Digits.cpp
@@ -0,0 +1,63 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+typedef long long                                        ll;
+typedef long double                                      ld;
+typedef pair <int, int>                                  pii;
+typedef pair <pii, int>                                  ppi;
+typedef pair <int, pii>                                  pip;
+typedef pair <pii, pii>                                  ppp;
+typedef pair <ll, ll>                                    pll;
+
+# define A                                               first
+# define B                                               second
+# define endl                                            '\n'
+# define sep                                             ' '
+# define all(x)                                          x.begin(), x.end()
+# define kill(x)                                         return cout << x << endl, 0
+# define SZ(x)                                           int(x.size())
+# define lc                                              id << 1
+# define rc                                              id << 1 | 1
+# define InTheNameOfGod                                  ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+ll power(ll a, ll b, ll md) {return (!b ? 1 : (b & 1 ? a * power(a * a % md, b / 2, md) % md : power(a * a % md, b / 2, md) % md));}
+
+const int xn = 1e2 + 10;
+const int xm = - 20 + 10;
+const int sq = 320;
+const int inf = 1e9 + 10;
+const ll INF = 1e18 + 10;
+const int mod = 998244353;
+const int base = 257;
+
+int main()
+{
+    InTheNameOfGod
+
+    string s;
+    cin>>s;
+    int cnt = 0;
+
+    if(s.length() == 1){
+        cout<<cnt<<endl;
+    }else{
+        while(s.length() > 1){
+            int sum = 0;
+            cnt++;
+            for(int i=0;i<s.length();i++){
+                stringstream str;
+                str << s[i];
+                int a;
+                str >>a;
+
+                sum += a;
+            }
+
+            s = to_string(sum);
+        }
+        cout<<cnt<<endl;
+
+    }
+
+    return 0;
+}
