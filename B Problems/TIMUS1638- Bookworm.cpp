553f76a10056ab00f4637ec00f1b98027ca98278
UVER <73294472+UVE-R@users.noreply.github.com>
2021-02-24 21:46:33 +0000

    Create TIMUS1638- Bookworm.cpp

---
+++ B Problems/TIMUS1638- Bookworm.cpp
@@ -0,0 +1,77 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+typedef long long                                        ll;
+typedef long double                                      ld;
+typedef pair <int, int>                                  pii;
+typedef pair <pii, int>                                  ppi;
+typedef pair <int, pii>                                  pip;
+typedef pair <pii, pii>                                  ppp;
+typedef pair <ll, ll>                                    pll;
+
+# define A                                               first
+# define B                                               second
+# define endl                                            '\n'
+# define sep                                             ' '
+# define kill(x)                                         return cout << x << endl, 0
+# define lc                                              id << 1
+# define rc                                              id << 1 | 1
+# define pb					                             push_back
+# define MP					                             make_pair
+# define P(x)				                             cout<<#x<<" = { "<<x<<" }\n"
+# define all(v)				                             ((v).begin()), ((v).end())
+# define sz(v)				                             ((int)((v).size()))
+# define clr(v, d)			                             memset(v, d, sizeof(v))
+# define rep(i, v)		                                 for(int i=0;i<sz(v);++i)
+# define lp(i, n)		                                 for(int i=0;i<(int)(n);++i)
+# define lpi(i, j, n)	                                 for(int i=(j);i<(int)(n);++i)
+# define lpd(i, j, n)	                                 for(int i=(j);i>=(int)(n);--i)
+# define InTheNameOfGod                                  ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+
+ll power(ll a, ll b, ll md) {return (!b ? 1 : (b & 1 ? a * power(a * a % md, b / 2, md) % md : power(a * a % md, b / 2, md) % md));}
+
+const int xn = 1e2 + 10;
+const int xm = - 20 + 10;
+const int sq = 320;
+const int inf = 1e9 + 10;
+const ll INF = 1e18 + 10;
+const int mod = 998244353;
+const int base = 257;
+const int OO = (int)1e6;
+const int maxim = 1e9;
+
+
+int main()
+{
+    InTheNameOfGod
+    //Volume width, cover width, start, end
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    int len = 0;
+    if(d==c){
+        len = a;
+    }else if ( d>c){
+        int between = d-c;
+
+        //Add len of books between
+        lpi(i,1,between){
+            len += 2*b + a;
+        }
+        //Add our cover front and the end cover's back
+        len += 2*b;
+
+    }else if(d<c){
+        int between = c-d;
+        //Add len of books between
+        lpi(i,1,between){
+            len += 2*b + a;
+        }
+        //Add len of our volume and back cover and end book's front cover and volume
+        len += 2*a + 2*b;
+    }
+
+    cout<<len<<endl;
+
+    return 0;
+}
