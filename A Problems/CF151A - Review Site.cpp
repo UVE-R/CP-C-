4f9c0f43bab90eb0403514e76a8a2b86e86cf658
UVER <73294472+UVE-R@users.noreply.github.com>
2021-04-14 23:47:30 +0100

    Create CF151A - Review Site.cpp

---
+++ A Problems/CF151A - Review Site.cpp
@@ -0,0 +1,68 @@
+# include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long                                        ll;
+typedef long double                                      ld;
+typedef pair <int, int>                                  pii;
+typedef pair <pii, int>                                  ppi;
+typedef pair <int, pii>                                  pip;
+typedef pair <pii, pii>                                  ppp;
+typedef pair <ll, ll>                                    pll;
+ 
+# define A                                               first
+# define B                                               second
+# define endl                                            '\n'
+# define sep                                             ' '
+# define kill(x)                                         return cout << x << endl, 0
+# define lc                                              id << 1
+# define rc                                              id << 1 | 1
+# define pb					                             push_back
+# define MP					                             make_pair
+# define P(x)				                             cout<<#x<<" = { "<<x<<" }\n"
+# define all(v)				                             ((v).begin()), ((v).end())
+# define sz(v)				                             ((int)((v).size()))
+# define clr(v, d)			                             memset(v, d, sizeof(v))
+# define rep(i, v)		                                 for(int i=0;i<sz(v);++i)
+# define lp(i, n)		                                 for(int i=0;i<(int)(n);++i)
+# define lpi(i, j, n)	                                 for(int i=(j);i<(int)(n);++i)
+# define lpd(i, j, n)	                                 for(int i=(j);i>=(int)(n);--i)
+# define InTheNameOfGod                                  ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+ 
+ll power(ll a, ll b, ll md) {return (!b ? 1 : (b & 1 ? a * power(a * a % md, b / 2, md) % md : power(a * a % md, b / 2, md) % md));}
+ 
+const int xn = 1e2 + 10;
+const int xm = - 20 + 10;
+const int sq = 320;
+const int inf = 1e9 + 10;
+const ll INF = 1e18 + 10;
+const int mod = 998244353;
+const int base = 257;
+const int OO = (int)1e6;
+const int maxim = 1e9;
+ 
+int main(){
+ 
+    int t;
+    cin>>t;
+ 
+    for(int i=0; i<t; i++){
+        int n;
+        cin>>n;
+ 
+        int cnt = 0;
+        for(int j=0; j<n ;j++){
+            int a;
+            cin>>a;
+            if(a==1 || a == 3){
+                cnt ++;
+            }
+        }
+ 
+        cout<<cnt<<endl;
+ 
+    }
+ 
+ 
+ 
+    return 0;
+}
